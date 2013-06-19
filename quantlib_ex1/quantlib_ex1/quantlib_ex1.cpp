836788a5877413f80555bbe62f6c40bbd494d713
lance <lance.bai@oregan.net>
2013-06-19 23:43:58 +0100

    tidy up

--- quantlib_ex1/quantlib_ex1/quantlib_ex1.cpp
+++ quantlib_ex1/quantlib_ex1/quantlib_ex1.cpp
@@ -1,7 +1,5 @@
-/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*!
- Copyright (C) 2006 Allen Kuo
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
